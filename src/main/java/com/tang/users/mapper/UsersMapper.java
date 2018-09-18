405f81e8744511499cc5306304c53203e9ffac3a
tangw <17375770327@163.com>
2018-09-18 15:13:45 +0800

    提交自动生成的基础代码

---
+++ src/main/java/com/tang/users/mapper/UsersMapper.java
@@ -0,0 +1,17 @@
+package com.tang.users.mapper;
+
+import com.tang.users.model.Users;
+
+public interface UsersMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    Users selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
---
+++ src/main/java/com/tang/users/model/Users.java
@@ -0,0 +1,138 @@
+package com.tang.users.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Users implements Serializable {
+    private Integer userId;
+
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    private String enabled;
+
+    private String accountNonExpired;
+
+    private String accountNonLocked;
+
+    private String credentialsNonExpired;
+
+    private String createBy;
+
+    private Date createTime;
+
+    private String remark;
+
+    private Date lastLoginTime;
+
+    private Date vipValidDate;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled == null ? null : enabled.trim();
+    }
+
+    public String getAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(String accountNonExpired) {
+        this.accountNonExpired = accountNonExpired == null ? null : accountNonExpired.trim();
+    }
+
+    public String getAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(String accountNonLocked) {
+        this.accountNonLocked = accountNonLocked == null ? null : accountNonLocked.trim();
+    }
+
+    public String getCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(String credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired == null ? null : credentialsNonExpired.trim();
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public Date getVipValidDate() {
+        return vipValidDate;
+    }
+
+    public void setVipValidDate(Date vipValidDate) {
+        this.vipValidDate = vipValidDate;
+    }
+}
